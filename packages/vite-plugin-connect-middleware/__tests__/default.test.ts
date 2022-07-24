4c67987635316501a4ecb0c6766bb6561c30f60c
macropygia <3162324+macropygia@users.noreply.github.com>
2022-07-24 14:30:59 +0900

    Add test to connect-middleware

---
+++ packages/vite-plugin-connect-middleware/__tests__/default.test.ts
@@ -0,0 +1,44 @@
+import path from 'path'
+
+import axios from 'axios'
+import { test } from 'vitest'
+import { createServer, preview } from 'vite'
+import type { UserConfig } from 'vite'
+
+import middlewarePlugin, { Middleware, Settings } from '../src/index.js'
+
+const srcdir = path.resolve(__dirname, 'src')
+const distdir = path.resolve(__dirname, 'dist')
+
+const middleware: Middleware = (req, _res, next) => {
+  if (req.url) {
+    if (req.url.startsWith('/app/')) {
+      req.url = '/app/'
+    }
+  }
+  return next()
+}
+
+const defaultConfig = (pluginConfig: Settings): UserConfig => ({
+  root: srcdir,
+  build: {
+    outDir: distdir,
+  },
+  plugins: [middlewarePlugin(pluginConfig)],
+})
+
+test('preview', async () => {
+  const server = await createServer(
+    defaultConfig({
+      dev: middleware,
+      preview: middleware,
+    })
+  )
+  await server.listen()
+  server.printUrls()
+})
+
+test('dev', async () => {
+  const server = await preview(defaultConfig(middleware))
+  server.printUrls()
+})
