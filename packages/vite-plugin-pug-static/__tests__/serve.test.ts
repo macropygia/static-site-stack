7013d446ef9f3cd9b6801d314198ef0be921e5b3
macropygia <3162324+macropygia@users.noreply.github.com>
2022-08-02 17:24:43 +0900

    Fix test for pug-static

--- packages/vite-plugin-pug-static/__tests__/serve.test.ts
+++ packages/vite-plugin-pug-static/__tests__/serve.test.ts
@@ -101,24 +101,7 @@ describe('serve', () => {
   })
   test('get /invalid/', async () => {
     await axios.get('http://localhost:5173/invalid/').catch((err: unknown) => {
-      if (err instanceof AxiosError)
-        expect(err.response?.data).toMatchInlineSnapshot(`
-            "
-                    <!DOCTYPE html>
-                    <html lang=\\"en\\">
-                      <head>
-                        <meta charset=\\"UTF-8\\" />
-                        <title>Error</title>
-                        <script type=\\"module\\">
-                          import { ErrorOverlay } from '/@vite/client'
-                          document.body.appendChild(new ErrorOverlay({\\"message\\":\\"/home/macropygia/static-site-stack/packages/vite-plugin-pug-static/__tests__/src/invalid/index.pug:5:7\\\\n    3|   head\\\\n    4|   body\\\\n  > 5|     h1あ\\\\n-------------^\\\\n    6| \\\\n\\\\nunexpected text \\\\\\"あ\\\\n\\\\\\"\\",\\"stack\\":\\"    at makeError (/home/macropygia/static-site-stack/node_modules/.pnpm/pug-error@2.0.0/node_modules/pug-error/index.js:34:13)\\\\n    at Lexer.error (/home/macropygia/static-site-stack/node_modules/.pnpm/pug-lexer@5.0.1/node_modules/pug-lexer/index.js:62:15)\\\\n    at Lexer.fail (/home/macropygia/static-site-stack/node_modules/.pnpm/pug-lexer@5.0.1/node_modules/pug-lexer/index.js:1629:10)\\\\n    at Lexer.advance (/home/macropygia/static-site-stack/node_modules/.pnpm/pug-lexer@5.0.1/node_modules/pug-lexer/index.js:1694:12)\\\\n    at Lexer.callLexerFunction (/home/macropygia/static-site-stack/node_modules/.pnpm/pug-lexer@5.0.1/node_modules/pug-lexer/index.js:1647:23)\\\\n    at Lexer.getTokens (/home/macropygia/static-site-stack/node_modules/.pnpm/pug-lexer@5.0.1/node_modules/pug-lexer/index.js:1706:12)\\\\n    at lex (/home/macropygia/static-site-stack/node_modules/.pnpm/pug-lexer@5.0.1/node_modules/pug-lexer/index.js:12:42)\\\\n    at Object.lex (/home/macropygia/static-site-stack/node_modules/.pnpm/pug@3.0.2/node_modules/pug/lib/index.js:104:9)\\\\n    at Function.loadString [as string] (/home/macropygia/static-site-stack/node_modules/.pnpm/pug-load@3.0.0/node_modules/pug-load/index.js:53:24)\\\\n    at compileBody (/home/macropygia/static-site-stack/node_modules/.pnpm/pug@3.0.2/node_modules/pug/lib/index.js:82:18)\\",\\"frame\\":\\"\\"}))
-                        </script>
-                      </head>
-                      <body>
-                      </body>
-                    </html>
-                  "
-          `)
+      if (err instanceof AxiosError) expect(err.response?.status).toBe(500)
     })
   })
   test('get /__inspect/', async () => {
